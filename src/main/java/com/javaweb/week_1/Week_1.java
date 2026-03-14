4c6b950a895b49c2466b95af907b47766991c9bd
Bondy <safaridung987@gmail.com>
2026-03-14 14:33:59 +0700

    up

---
+++ src/main/java/com/javaweb/week_1/Week_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.javaweb.week_1;
+
+/**
+ *
+ * @author Bondy
+ */
+public class Week_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
