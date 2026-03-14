d70eee616161d66469e85a9cb895e50b9e6e801f
Bondy <safaridung987@gmail.com>
2026-03-14 14:57:43 +0700

    update bai 1 class

--- src/main/java/com/javaweb/week_1/classwork/Bai1.java
+++ src/main/java/com/javaweb/week_1/classwork/Bai1.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.javaweb.week_1.classwork;
 import java.util.Scanner;
-/**
- *
- * @author Bondy
- */
+
 public class Bai1 {
     public static final double PI = 3.14159;
     
