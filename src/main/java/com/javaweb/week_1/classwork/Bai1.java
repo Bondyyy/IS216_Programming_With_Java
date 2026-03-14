7182dd93830553b47ddf201299b957191e6a6db8
Bondy <safaridung987@gmail.com>
2026-03-14 14:57:07 +0700

    update bai 1 class

--- src/main/java/com/javaweb/week_1/classwork/Bai1.java
+++ src/main/java/com/javaweb/week_1/classwork/Bai1.java
@@ -9,15 +9,14 @@ import java.util.Scanner;
  * @author Bondy
  */
 public class Bai1 {
-    public static void main(String[] args){
-        Scanner scanIn=new Scanner(System.in); 
-        String ten = ""; // Tên 
-        int tuoi; // Tuổi 
-        System.out.println("Nhap Ten va Tuoi: "); 
-        ten = scanIn.nextLine();  
-        tuoi = scanIn.nextInt(); 
-        System.out.println("Ten " + ten + "!"); 
-        System.out.println("Tuoi " + tuoi + "!");
-    }
+    public static final double PI = 3.14159;
     
+    public static void main(String[] args){
+        Scanner scanIn = new Scanner(System.in);
+        double banKinh = 0.0;
+       
+        System.out.println("Nhap vao ban kinh: ");
+        banKinh = scanIn.nextInt();
+        System.out.println("Chu vi cua hinh tron la "+ banKinh * 2 * PI);
+    }
 }
