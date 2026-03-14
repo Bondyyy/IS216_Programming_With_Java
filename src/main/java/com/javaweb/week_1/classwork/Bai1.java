dcde8410243e4e554a281fe29798a85779cded91
Bondy <safaridung987@gmail.com>
2026-03-14 16:01:30 +0700

    add bai 2 and bai 3

--- src/main/java/com/javaweb/week_1/classwork/Bai1.java
+++ src/main/java/com/javaweb/week_1/classwork/Bai1.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 public class Bai1 {
     public static final double PI = 3.14159;
     
+    public static double tinhChuViHinhTron (double banKinh){
+        return banKinh * 2 * PI;
+    }
+    
     public static void main(String[] args){
         Scanner scanIn = new Scanner(System.in);
         double banKinh = 0.0;
        
         System.out.println("Nhap vao ban kinh: ");
-        banKinh = scanIn.nextInt();
-        System.out.println("Chu vi cua hinh tron la "+ banKinh * 2 * PI);
+        banKinh = scanIn.nextDouble();
+        System.out.println("Chu vi cua hinh tron la "+ tinhChuViHinhTron(banKinh));
     }
 }
